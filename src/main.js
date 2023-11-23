ab483c9d619f9d4683a902cc845a401449803dd8
Phileaux Puni Vaesau <fvaesau3@gmail.com>
2023-11-23 10:23:33 -0800

    Added some minor changes in addition to add a js file to project

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// let title = document.querySelector(".big__header");
+
+// title.innerHTML = "Hello";
+
+let randomArr = ["One", "Two", "Three"];
+
+let randomNum = Math.floor(Math.random()*100);
+console.log(randomNum);
+
+
